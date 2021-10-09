7f799ee4ceb2aef7ee8a4af64261cade1b8dd0a1
Anmol Singh Yadav <53969258+IamLucif3r@users.noreply.github.com>
2021-10-09 19:33:07 +0530

    Updated Timestamp - time.Time

--- models/user.go
+++ models/user.go
@@ -6,12 +6,14 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+// For the User Details
 type User struct {
 	ID       bson.ObjectId `json:"id" bson:"_id"`
 	Name     string        `json:"name" bson:"name"`
 	Email    string        `json:"email" bson:"email"`
 	Password string        `json:"password" bson:"password"`
 }
+// For the Posts
 type Post struct {
 	ID        bson.ObjectId `json:"pid" bson:"_pid"`
 	Caption   string        `json:"caption" bson:"caption"`
